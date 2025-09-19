6e00b79e07a2cd36e7a6bddace75b3ddd3f95fc5
Darshan Lamichhane <lamichhane3848@gmail.com>
2025-09-19 18:15:54 +0545

    Test 3

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-console.log("Script loaded successfully.");
-<<<<<<< HEAD
+console.log("Script loaded successfully.")
 console.log("TEST 2")
-=======
 console.log("Test 1")
->>>>>>> cca9ba158a282fdb5f5a521aa82ac24e251a802f
+console.log("Test 3")
